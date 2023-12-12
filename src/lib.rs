edc91f1f7f354c85599e8f8ccbaf1b6388d05427
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-12-12 13:30:42 +0100

    Add structs

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,19 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+
+/// Stores the prefix and local unique identifier
+/// for a compact URI (CURIE)
+pub struct Reference {
+    prefix: String,
+    identifier: String,
+}
+
+
+pub struct Record {
+    curie_prefix: String,
+    uri_prefix: String,
+    curie_prefix_synonyms: Vec<String>,
+    uri_prefix_synonyms: Vec<String>,
+}
