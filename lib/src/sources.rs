a0589844a3b6e97e883ed32510bd03d86555c683
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-12-15 13:22:36 +0100

    Update sources.rs

--- lib/src/sources.rs
+++ lib/src/sources.rs
@@ -1,6 +1,9 @@
 use crate::{error::CuriesError, Converter};
 
-/// Get the latest OBO Foundry context.
+/// Get the latest OBO Foundry JSON-LD context.
+///
+///
+///
 pub async fn get_obo_converter() -> Result<Converter, CuriesError> {
     Converter::from_jsonld("http://purl.obolibrary.org/meta/obo_context.jsonld").await
 }
