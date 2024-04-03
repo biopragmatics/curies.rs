0e7c00e6531487736641db91c12534f6a890dbb5
Vincent Emonet <vincent.emonet@gmail.com>
2024-04-03 14:12:32 +0200

    fix R tests

--- scripts/test-r.sh
+++ scripts/test-r.sh
@@ -13,11 +13,13 @@ for arg in "$@"; do
 done
 
 if [ "$INSTALL_DEPS" = true ]; then
-    Rscript -e 'install.packages("usethis")'
-    Rscript -e 'install.packages("devtools")'
-    Rscript -e 'install.packages("testthat")'
-    Rscript -e 'install.packages("rextendr")'
+    Rscript -e 'install.packages("usethis"); install.packages("devtools"); install.packages("testthat"); install.packages("rextendr"); rextendr::document("./r"); library(testthat); test_dir("r/tests");'
+    # NOTE: the packages installed in separate Rscript commands are not available in the next command, so we need to install them all in one command
+    # Rscript -e 'install.packages("usethis")'
+    # Rscript -e 'install.packages("devtools")'
+    # Rscript -e 'install.packages("testthat")'
+    # Rscript -e 'install.packages("rextendr")'
     # Rscript -e 'remotes::install_github("extendr/rextendr")'
+else
+    Rscript -e 'rextendr::document("./r"); library(testthat); test_dir("r/tests");'
 fi
-
-Rscript -e 'rextendr::document("./r"); library(testthat); test_dir("r/tests");'
